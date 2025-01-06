c870d4338cc564a76378e278649f96b79573999f
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2025-01-06 14:19:09 +0300

    update

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+'use client'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 import { AiFillGithub } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
 
+
+const defaultFormData = {
+    email: '',
+    name: '',
+    password: '',
+  };
+
 const Auth = () => {
+    const [formData, setFormData] = useState(defaultFormData);
+
     const inputStyles =
     'border border-gray-300 sm:text-sm text-black rounded-lg block w-full p-2.5 focus:outline-none';
-
+   
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+      };
+    const handleSubmit= async (event:FormEvent<HTMLElement>)  =>{
+        event.preventDefault();
+        try {
+           console.log(formData) 
+        } catch (error) {
+            console.log(error)
+        }finally{
+      setFormData(defaultFormData);
+        }
+    }
   return (
     <section className='container mx-auto'>
          <div className='p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto'>
@@ -20,18 +44,46 @@ const Auth = () => {
           </span>
           </div>
 
-          <form className='space-y-4 md:space-y-6' >
+          <form className='space-y-4 md:space-y-6' onSubmit={handleSubmit}>
           <input
             type='email'
             name='email'
             placeholder='name@company.com'
             required
             className={inputStyles}
+            value={formData.email}
+            onChange={handleInputChange}
           />
-          
-         
+          <input
+            type='text'
+            name='name'
+            placeholder='John Doe'
+            required
+            className={inputStyles}
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type='password'
+            name='password'
+            placeholder='password'
+            required
+            minLength={6}
+            className={inputStyles}
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        
+          <button
+            type='submit'
+            className='w-full bg-tertiary-dark focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+          >
+            Sign Up
+          </button>
         </form>
-
+        <button  className='text-blue-700 underline'>
+          login
+        </button>
 
           </div>
        </section>
