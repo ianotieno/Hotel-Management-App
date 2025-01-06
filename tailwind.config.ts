f3ddc4be0b2e3d84c7cf47b6f8d8b42eb8a3fc2d
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2025-01-06 11:51:57 +0300

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
-
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { fontFamily } = require('tailwindcss/defaultTheme');
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +10,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#038C7F',
+        secondary: '#F2C641',
+        tertiary: {
+          dark: '#F27405',
+          light: '#F2C641',
+        }
+      },fontFamily: {
+        poppins: ['var(--font-poppins)', ...fontFamily.sans],
       },
     },
   },
