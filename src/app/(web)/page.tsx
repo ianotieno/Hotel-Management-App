cc6f2dc7424dddbc8bf1c65e9bf83c67520b36d8
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2025-01-07 17:07:01 +0300

    update newsletter

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,7 +1,9 @@
 
 
+import Gallery from '@/Components/Gallery/Gallery'
 import HeroSection from '@/Components/HeroSection/HeroSection'
 import PageSearch from '@/Components/pageSearch/PageSearch'
+import NewsLetter from '@/Components/NewsLetter/NewsLetter'
 import React from 'react'
 
 const Home = () => {
@@ -9,6 +11,8 @@ const Home = () => {
     <>
     <HeroSection/>
     <PageSearch/>
+    <Gallery/>
+    <NewsLetter/>
     </>
   )
 }
