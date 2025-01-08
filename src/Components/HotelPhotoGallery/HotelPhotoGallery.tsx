3d87d7a106ff775523165efbc308a6607fd846a3
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2025-01-08 11:24:52 +0300

    update

--- src/Components/HotelPhotoGallery/HotelPhotoGallery.tsx
+++ src/Components/HotelPhotoGallery/HotelPhotoGallery.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 import { Image as ImageType } from '@/model/room';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
-
+import { MdCancel } from 'react-icons/md';
 
 const HotelPhotoGallery: FC<{ photos: ImageType[] }> = ({ photos }) => {
   const [currenPhotoIndex, setCurrentPhotoIndex] = useState(0);
@@ -16,7 +16,7 @@ const HotelPhotoGallery: FC<{ photos: ImageType[] }> = ({ photos }) => {
     setShowModal(true);
   };
 
- 
+  const closeModal = () => setShowModal(false);
 
   const handlePrevious = () => {
     setCurrentPhotoIndex(prevIndex =>
@@ -104,7 +104,40 @@ const HotelPhotoGallery: FC<{ photos: ImageType[] }> = ({ photos }) => {
           )}
         </div>
 
-        
+        {showModal && (
+          <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-90 z-[55]'>
+            <div className='h-[75vh] w-[320px] md:w-[700px] relative'>
+              <Image
+                src={photos[currenPhotoIndex].url}
+                alt={`Room Photo ${currenPhotoIndex + 1}`}
+                width={150}
+                height={150}
+                className='img'
+              />
+              <div className='flex justify-between items-center py-3'>
+                <div className='flex space-x-2 items-center text-white'>
+                  <FaArrowLeft
+                    className='cursor-pointer'
+                    onClick={handlePrevious}
+                  />
+                  <FaArrowRight
+                    className='cursor-pointer'
+                    onClick={handleNext}
+                  />
+                </div>
+                <span className='text-white text-sm'>
+                  {currenPhotoIndex + 1} / {photos.length}
+                </span>
+              </div>
+              <button
+                className='absolute top-2 right-2 text-white text-lg'
+                onClick={closeModal}
+              >
+                <MdCancel className='font-medium text-2xl text-tertiary-dark' />
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
