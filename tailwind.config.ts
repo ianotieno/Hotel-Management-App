35158ea1e0cfe138b642b75546caea63010d3186
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2025-01-21 08:23:59 +0300

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,9 @@ export default {
   important: true,
   darkMode: 'class',
   content: [
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
