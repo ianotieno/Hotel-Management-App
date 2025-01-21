25f086d26e339608bd5c74b2b943bccebea8aa94
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2025-01-21 19:32:34 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com', 'images.unsplash.com','media.istockphoto.com','plus.unsplash.com'],
-    unoptimized: true,
+   
   },
   
 };
