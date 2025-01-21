f8ea6ab687ea24a9d60e36ded2c46d6cdcb44551
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2025-01-21 08:36:07 +0300

    update

--- src/Components/HeroSection/ServerComponent.tsx
+++ src/Components/HeroSection/ServerComponent.tsx
@@ -17,8 +17,8 @@ export const section2 = (
         {/* Hero Image 1 */}
         <div className="rounded-2xl overflow-hidden h-48 shadow-lg transition-transform duration-300 hover:scale-105">
             <Image
-                src='/images/hero-1.jpeg'
-                alt='hero-1'
+                src="/images/hero-1.jpeg"
+                alt="hero-1"
                 width={300}
                 height={300}
                 className="img object-cover w-full h-full"
@@ -29,7 +29,7 @@ export const section2 = (
         <div className="grid grid-cols-2 gap-8 h-48">
             <div className="rounded-2xl overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105">
                 <Image
-                    src='/images/hero-2.jpeg'
+                    src="/images/hero-2.jpeg"
                     alt='hero-2'
                     width={300}
                     height={300}
@@ -38,7 +38,7 @@ export const section2 = (
             </div>
             <div className="rounded-2xl overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105">
                 <Image
-                    src='/images/hero-3.jpeg'
+                    src="/images/hero-3.jpeg"
                     alt='hero-3'
                     width={300}
                     height={300}
